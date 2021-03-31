3f62324e90390ce73e2830b0ed1d6de2b317ea86
Ricardo <ric.morales22@gmail.com>
2021-03-31 10:39:08 -0400

    Remove several obsolete scripts that can't be used within Cocos

--- persistentData/registerableScriptableObject.ts
+++
@@ -1,8 +0,0 @@
-
-import { _decorator, Component, Node, Script } from 'cc';
-const { ccclass, property } = _decorator;
-
-@ccclass('RegisterableScriptableObject')
-export class RegisterableScriptableObject extends Script {
-    
-}
