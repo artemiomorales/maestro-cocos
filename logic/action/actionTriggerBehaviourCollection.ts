f336301d4bf3882289b524d8a023fd3fe24a9399
Ricardo <ric.morales22@gmail.com>
2022-02-14 22:36:30 -0500

    Add handling to make sure actionTriggerBehaviourColleciton events only fire while going forward during animations (reverse events have not been implemented due to a Cocos bug

--- logic/action/actionTriggerBehaviourCollection.ts
+++ logic/action/actionTriggerBehaviourCollection.ts
@@ -1,5 +1,7 @@
 
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, find } from 'cc';
+import { CONSTANTS } from '../../constants';
+import AppSettings from '../../persistentData/appSettings';
 import { ActionTriggerBehaviour } from './actionTriggerBehaviour';
 const { ccclass, property } = _decorator;
 
@@ -15,6 +17,24 @@ export class ActionTriggerBehaviourItem {
     this._key = value;
   }
 
+  @property({visible: true})
+  private _executeOnForward = true;
+  public get executeOnForward() {
+    return this._executeOnForward;
+  }
+  public set executeOnForward(value: boolean) {
+    this._executeOnForward = value;
+  }
+
+  // @property({visible: true})
+  // private _executeOnReverse = false;
+  // public get executeOnReverse() {
+  //   return this._executeOnReverse;
+  // }
+  // public set executeOnReverse(value: boolean) {
+  //   this._executeOnReverse = value;
+  // }
+
   @property({type:ActionTriggerBehaviour, visible: true})
   public _actionTriggerBehaviour: ActionTriggerBehaviour = null!;
   public get actionTriggerBehaviour() {
@@ -29,6 +49,15 @@ export class ActionTriggerBehaviourItem {
 @ccclass('ActionTriggerBehaviourCollection')
 export default class ActionTriggerBehaviourCollection extends Component {
 
+  @property({type: Node, visible: false})
+  public appSettingsNode: Node = null!;
+  private appSettings: AppSettings = null!;
+
+  start () {
+    this.appSettingsNode = find(CONSTANTS.APP_SETTINGS_PATH) as Node;
+    this.appSettings = this.appSettingsNode.getComponent(AppSettings) as AppSettings;
+  }
+
   @property({type:[ActionTriggerBehaviourItem], visible: true})
   public _actionTriggerBehaviours: ActionTriggerBehaviourItem[] = [];
   public get actionTriggerBehaviours() {
@@ -39,10 +68,25 @@ export default class ActionTriggerBehaviourCollection extends Component {
   }
 
   callPerformActionsByKey(key: string) {
-    console.log(key);
     const item = this.actionTriggerBehaviours.find(x => x.key === key);
+
     if(item) {
-      item.actionTriggerBehaviour.callPerformActions(this.node.name);
+      // Execute action depending on action settings and whether we
+      // are moving forward or backward
+      if(this.appSettings.getIsReversing(this.node) === false) {
+        if(item.executeOnForward === true) {
+          item.actionTriggerBehaviour.callPerformActions(this.node.name);
+        }
+      } else {
+        // Due to a bug in Cocos Creator wherein reverse events are fired
+        // when they're not supposed to, reverse events are not currently supported
+
+        // if(item.executeOnReverse === true && Math.abs(this.sequenceController.currentTime - item.reverseTimeSignature) < .05) {
+        //   console.log("executing");
+        //   item.actionTriggerBehaviour.callPerformActions(this.node.name);
+        // }
+      }
+        
     } else {
       throw "Unable to find key in ActionTriggerBehaviourCollection";
     }
